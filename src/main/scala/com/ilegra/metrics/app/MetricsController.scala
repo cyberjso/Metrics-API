698b5ee2e1f8f22b26d89adba201ee947b7b052f
Jackson <cyber.jso@gmail.com>
2014-02-10 23:57:58 -0200

    Removing replicated code

--- src/main/scala/com/ilegra/metrics/app/MetricsController.scala
+++ src/main/scala/com/ilegra/metrics/app/MetricsController.scala
@@ -8,10 +8,12 @@ import com.twitter.finatra.Controller
 import com.ilegra.metrics.app.model.Card
 import scala.collection.immutable.HashMap
 import java.net.URL
+import java.text.SimpleDateFormat
+import com.twitter.finatra.Request
 
 class MetricsController extends Controller {
-	val baseUrl = "/api/v1/teams/"
 	val hostName = "http://localhost:7070"
+	val baseUrl = "/api/v1/teams/"
 	  
 	get(baseUrl + "boards") { request =>
 	  render.json(List(new URL(hostName + baseUrl + "boards/1"), 
@@ -20,47 +22,42 @@ class MetricsController extends Controller {
 	}
 	
 	get(baseUrl + "boards/:boardId") {request =>
-	  	val boardId = request.routeParams.get("boardId").get
-		render.json(Board(id = boardId, 
+		render.json(Board(id = getParameterValue(request, "boardId"), 
 		    			  name = "Board Name", 
-		    			  queues = List(new URI(hostName + baseUrl + "boards/" + boardId + "/queues/1"),
-		    					  	    new URI(hostName + baseUrl + "boards/" + boardId + "/queues/2")))).toFuture
+		    			  queues = List(new URI(hostName + baseUrl + "boards/" + getParameterValue(request, "boardId") + "/queues/1"),
+		    					  	    new URI(hostName + baseUrl + "boards/" + getParameterValue(request, "boardId") + "/queues/2")))).toFuture
 	}
 	
 	get(baseUrl + "boards/:boardId/queues") {request =>
-	  	val boardId = request.routeParams.get("boardId").get
-		render.json(List(new URI(hostName + baseUrl + "boards/" + boardId + "/queues/1"),
-						 new URI(hostName + baseUrl + "boards/" + boardId + "/queues/2"))).toFuture
+		render.json(List(new URI(hostName + baseUrl + "boards/" + getParameterValue(request, "boardId") + "/queues/1"),
+						 new URI(hostName + baseUrl + "boards/" + getParameterValue(request, "boardId") + "/queues/2"))).toFuture
 	}
 
 	get(baseUrl + "boards/:boardId/queues/:queueId") { request =>
-	  val boardId = request.routeParams.get("boardId").get
-	  val queueId = request.routeParams.get("queueId").get
-	  render.json(Queue(id    = queueId, 
+	  render.json(Queue(id    = getParameterValue(request, "queueId"), 
 	      				name  = "Some Queue Name", 
-	      				cards = List(new URI(hostName + baseUrl + "boards/" + boardId +  "/queues/" + queueId + "/cards/1"),
-	      							 new URI(hostName + baseUrl + "boards/" + boardId +  "/queues/" + queueId + "/cards/2")))).toFuture
+	      				cards = List(new URI(hostName + baseUrl + "boards/" + getParameterValue(request, "boardId") +  "/queues/" + getParameterValue(request, "queueId") + "/cards/1"),
+	      							 new URI(hostName + baseUrl + "boards/" + getParameterValue(request, "boardId") +  "/queues/" + getParameterValue(request, "queueId") + "/cards/2")))).toFuture
 	}
 	
 	get(baseUrl + "boards/:boardId/queues/:queueId/cards")	 {request =>
-		  val boardId = request.routeParams.get("boardId").get
-		  val queueId = request.routeParams.get("queueId").get
-		  render.json(List(new URI(hostName + baseUrl + "boards/" + boardId + "queues/1/cards/1"),
-				  		   new URI(hostName + baseUrl + "boards/" + boardId + "queues/1/cards/2"))).toFuture
+		  render.json(List(new URI(hostName + baseUrl + "boards/" + getParameterValue(request, "boardId") + "queues/" + getParameterValue(request, "queueId") + "/cards/1"),
+				  		   new URI(hostName + baseUrl + "boards/" + getParameterValue(request, "boardId") + "queues/" + getParameterValue(request, "queueId") +"/cards/2"))).toFuture
 	}
 	
 	get(baseUrl + "boards/:boardId/queues/:queueId/cards/:cardId") {request =>
-		  val cardId = request.routeParams.get("cardId").get
-		  render.json(Card(id = cardId, 
-		      			   name = "Some user story name", 
-		      			   points ="3", 
-		      			   startDate = new Date(System.currentTimeMillis()),
-		      			   endDate  = new Date(System.currentTimeMillis()),
-		      			   leadTime = HashMap("testAndCode" -> 3, "codeReview" -> 4, "acceptance" -> 1 ), 
-		      			   testInfo = HashMap("unitTests" -> 1, "integrationTests" -> 4, "integrationTestsFixed" -> 0 )
+		  def getMockedDate() = new SimpleDateFormat("dd-MMM-yyyy").format(new Date(System.currentTimeMillis()))
+		  
+		  render.json(Card(id = getParameterValue(request, "cardId"), 
+		      			   name      = "Some user story name", 
+		      			   points    = "3", 
+		      			   startDate = getMockedDate(),
+		      			   endDate   = getMockedDate(),
+		      			   leadTime  = HashMap("testAndCode" -> 3, "codeReview" -> 4, "acceptance" -> 1 ), 
+		      			   testInfo  = HashMap("unitTests" -> 1, "integrationTests" -> 4, "integrationTestsFixed" -> 0 )
 		      			   )).toFuture
-	  
 	}
 	
+	def getParameterValue(request: Request, parameterName: String) =  request.routeParams.get(parameterName).get
 	
 }
--- src/main/scala/com/ilegra/metrics/app/model/Card.scala
+++ src/main/scala/com/ilegra/metrics/app/model/Card.scala
@@ -1,5 +1,3 @@
 package com.ilegra.metrics.app.model
 
-import java.util.Date
-
-case class Card(id: String, name: String, points: String, startDate: Date, endDate: Date, leadTime: Map[String, Integer], testInfo: Map[String, Integer])
+case class Card(id: String, name: String, points: String, startDate: String, endDate: String, leadTime: Map[String, Integer], testInfo: Map[String, Integer])
